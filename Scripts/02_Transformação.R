cc12ac783b71985d9d696e63692f250b0c6224c9
Vitor Villarino <vitor.villarino@gmail.com>
2019-08-07 21:49:22 -0300

    #07/08

--- Scripts/02_Transformação.R
+++ Scripts/02_Transformação.R
@@ -84,16 +84,36 @@ Proagro_soja_mun_seguro<- data_Proagro %>%
     VL_SUBVENCAO_FEDERAL = 0,
     RECEITA_ESTIMADA = sum(RBE)
   )
-Proagro_soja_mun_programa$Area_Media               = Proagro_soja_mun_programa$NR_AREA_TOTAL/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Limite_Medio             = Proagro_soja_mun_programa$VL_LIMITE_GARANTIA/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Premio_Liquido_Medio     = Proagro_soja_mun_programa$VL_PREMIO_LIQUIDO/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Subvencao_Media          = Proagro_soja_mun_programa$VL_SUBVENCAO_FEDERAL/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Receita_Media            = Proagro_soja_mun_programa$RECEITA_ESTIMADA/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Premio_Area              = Proagro_soja_mun_programa$VL_PREMIO_LIQUIDO/Proagro_soja_mun_programa$NR_AREA_TOTAL
-Proagro_soja_mun_programa$Tipo                     = Proagro_soja_mun_programa$SEGURO
+Proagro_soja_mun_seguro$Area_Media               = Proagro_soja_mun_seguro$NR_AREA_TOTAL/Proagro_soja_mun_seguro$num_seguros
+Proagro_soja_mun_seguro$Limite_Medio             = Proagro_soja_mun_seguro$VL_LIMITE_GARANTIA/Proagro_soja_mun_seguro$num_seguros
+Proagro_soja_mun_seguro$Premio_Liquido_Medio     = Proagro_soja_mun_seguro$VL_PREMIO_LIQUIDO/Proagro_soja_mun_seguro$num_seguros
+Proagro_soja_mun_seguro$Subvencao_Media          = Proagro_soja_mun_seguro$VL_SUBVENCAO_FEDERAL/Proagro_soja_mun_seguro$num_seguros
+Proagro_soja_mun_seguro$Receita_Media            = Proagro_soja_mun_seguro$RECEITA_ESTIMADA/Proagro_soja_mun_seguro$num_seguros
+Proagro_soja_mun_seguro$Premio_Area              = Proagro_soja_mun_seguro$VL_PREMIO_LIQUIDO/Proagro_soja_mun_seguro$NR_AREA_TOTAL
+Proagro_soja_mun_seguro$Tipo                     = Proagro_soja_mun_seguro$SEGURO
 
 
 
+
+PSR_Proagro_Union_Mun <- union_all(
+  x = Proagro_soja_mun %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
+  y = PSR_soja_mun %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
+)
+
+
+
+PSR_Proagro_Union_Mun_Programa <- union_all(
+  x = Proagro_soja_mun_programa %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
+  y = PSR_soja_mun_programa %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
+)
+
+
+
+PSR_Proagro_lado_lado_mun <-  left_join(
+  x = PSR_soja_mun,
+  y = Proagro_soja_mun,
+  by =c("Cod_Municipio" = "Cod_Municipio")
+)
 
 
 
@@ -150,5 +170,18 @@ Proagro_soja_regiao$Premio_Area              = Proagro_soja_regiao$VL_PREMIO_LIQ
 Proagro_soja_regiao$Tipo                     = "Proagro"
 
 
+
+PSR_Proagro_Union_regiao <- union_all(
+  x = Proagro_soja_regiao %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
+  y = PSR_soja_regiao %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
+)
+
+
+PSR_Proagro_lado_lado_regiao <-  left_join(
+  x = PSR_soja_regiao,
+  y = Proagro_soja_regiao,
+  by =c("Cod_Microrregiao" = "Cod_Microrregiao")
+)
+PSR_Proagro_lado_lado_regiao <- PSR_Proagro_lado_lado_regiao[!is.na(PSR_Proagro_lado_lado_regiao$NR_AREA_TOTAL.y),]
 
 
--- Scripts/EstudoProagro.R
+++ Scripts/EstudoProagro.R
@@ -23,17 +23,6 @@ ggplot(PSR_soja_mun) +
   theme_minimal()
 
 
-PSR_Proagro_Union_Mun <- union_all(
-        x = Proagro_soja_mun %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
-        y = PSR_soja_mun %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
-)
-
-
-
-PSR_Proagro_Union_Mun_Programa <- union_all(
-  x = Proagro_soja_mun_programa %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
-  y = PSR_soja_mun_programa %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
-)
 
 
 Test_Disp <- PSR_Proagro_Union_Mun 
@@ -85,23 +74,14 @@ Proagro_soja_mun_programa %>%
 
 
 # Diferenca entre
-PSR_Proagro_lado_lado_mun <-  left_join(
-                  x = PSR_soja_mun,
-                  y = Proagro_soja_mun,
-                  by =c("Cod_Municipio" = "Cod_Municipio")
-)
-PSR_Proagro_lado_lado_mun <- PSR_Proagro_lado_lado_mun[!is.na(PSR_Proagro_lado_lado_mun$NR_AREA_TOTAL.y),]
+
 
 PSR_Proagro_lado_lado_mun$Diferenca_Premio <- PSR_Proagro_lado_lado_mun$Premio_Area.x - PSR_Proagro_lado_lado_mun$Premio_Area.y
 PSR_Proagro_lado_lado_mun$Diferenca_Premio_Perc <- PSR_Proagro_lado_lado_mun$Diferenca_Premio/PSR_Proagro_lado_lado_mun$Premio_Area.x 
 
 
 
-PSR_Proagro_lado_lado_mun <- left_join(
-                                x = PSR_Proagro_lado_lado_mun,
-                                y = municipios,
-                                by = c("Cod_Municipio" = "Cod_Municipio" )
-)
+
 
 stby(data = PSR_Proagro_lado_lado_mun[,c("Diferenca_Premio","Sigla_UF")], 
      INDICES = PSR_Proagro_lado_lado_mun$Sigla_UF, 
@@ -125,7 +105,6 @@ ggplot(PSR_Proagro_lado_lado_mun) +
   geom_histogram(bins = 30L, fill = "#0c4c8a") +
   theme_minimal()
 
-
 openxlsx::write.xlsx(PSR_Proagro_lado_lado_mun, "df.xlsx")
 shell.exec("df.xlsx")
 
@@ -134,12 +113,6 @@ shell.exec("df.xlsx")
 
 
 
-
-
-PSR_Proagro_Union_regiao <- union_all(
-  x = Proagro_soja_regiao %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
-  y = PSR_soja_regiao %>%  select(-c("Produtividade_Seg_Media", "PE_NIVEL_COBERTURA", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "Cobertura_Media", "NR_PRODUTIVIDADE_ESTIMADA"))
-)
 
 
 cor(PSR_Proagro_Union_regiao[,8:12])
@@ -157,12 +130,7 @@ PSR_Proagro_Union_regiao %>%
   theme_minimal()
 
 
-PSR_Proagro_lado_lado_regiao <-  left_join(
-  x = PSR_soja_regiao,
-  y = Proagro_soja_regiao,
-  by =c("Cod_Microrregiao" = "Cod_Microrregiao")
-)
-PSR_Proagro_lado_lado_regiao <- PSR_Proagro_lado_lado_regiao[!is.na(PSR_Proagro_lado_lado_regiao$NR_AREA_TOTAL.y),]
+
 
 PSR_Proagro_lado_lado_regiao$Diferenca_Premio <- (PSR_Proagro_lado_lado_regiao$Premio_Area.x - PSR_Proagro_lado_lado_regiao$Premio_Area.y)
 
