4a2f16d10a2bf460555a52610ca7c4c008c692a8
Vitor Villarino <vitor.villarino@gmail.com>
2019-08-15 23:49:44 -0300

    #COMMIT 15/08

--- Scripts/02_Transformação.R
+++ Scripts/02_Transformação.R
@@ -5,7 +5,7 @@ PSR_soja_mun <- data_PSR %>%
   summarise(
     num_seguros = n(),
     NR_AREA_TOTAL = sum(NR_AREA_TOTAL),
-    VL_LIMITE_GARANTIA = sum(VL_LIMITE_GARANTIA),
+    VL_TOTAL = sum(VL_LIMITE_GARANTIA),
     VL_PREMIO_LIQUIDO = sum(VL_PREMIO_LIQUIDO),
     VL_SUBVENCAO_FEDERAL = sum(VL_SUBVENCAO_FEDERAL),
     VL_PREMIO_PAGO = sum(VL_PREMIO_PAGO),
@@ -18,14 +18,14 @@ PSR_soja_mun <- data_PSR %>%
 PSR_soja_mun$VL_PREMIO_PAGO           = PSR_soja_mun$VL_PREMIO_LIQUIDO - PSR_soja_mun$VL_SUBVENCAO_FEDERAL
 PSR_soja_mun$VL_PREMIO_PAGO_Medio     = PSR_soja_mun$VL_PREMIO_PAGO/PSR_soja_mun$num_seguros
 PSR_soja_mun$Area_Media               = PSR_soja_mun$NR_AREA_TOTAL/PSR_soja_mun$num_seguros
-PSR_soja_mun$Limite_Medio             = PSR_soja_mun$VL_LIMITE_GARANTIA/PSR_soja_mun$num_seguros
+PSR_soja_mun$VL_TOTAL_Medio           = PSR_soja_mun$VL_TOTAL/PSR_soja_mun$num_seguros
 PSR_soja_mun$Premio_Liquido_Medio     = PSR_soja_mun$VL_PREMIO_LIQUIDO/PSR_soja_mun$num_seguros
 PSR_soja_mun$Subvencao_Media          = PSR_soja_mun$VL_SUBVENCAO_FEDERAL/PSR_soja_mun$num_seguros
 PSR_soja_mun$Produtividade_Media      = PSR_soja_mun$NR_PRODUTIVIDADE_ESTIMADA/PSR_soja_mun$num_seguros
 PSR_soja_mun$Produtividade_Seg_Media  = PSR_soja_mun$NR_PRODUTIVIDADE_SEGURADA/PSR_soja_mun$num_seguros
 PSR_soja_mun$Premio_Area              = PSR_soja_mun$VL_PREMIO_LIQUIDO/PSR_soja_mun$NR_AREA_TOTAL
 PSR_soja_mun$INDICE_SINISTRO          = PSR_soja_mun$QT_SINISTROS/PSR_soja_mun$num_seguros
-PSR_soja_mun$VL_SINISTO_MEDIO         = PSR_soja_mun$VL_SINISTROS/PSR_soja_mun$QT_SINISTROS
+PSR_soja_mun$VL_SINISTRO_MEDIO        = PSR_soja_mun$VL_SINISTROS/PSR_soja_mun$QT_SINISTROS
 PSR_soja_mun$Tipo                     = "PSR"
 
 PSR_soja_mun_programa <- PSR_soja_mun
@@ -35,7 +35,6 @@ PSR_soja_mun_programa$PROGRAMA = 'PSR'
 
 
 #Proagro - Agrupando Municipio Produto
-
 Proagro_soja_mun <- data_Proagro %>% 
   filter(Produto_Padronizado == "SOJA") %>% 
   group_by(Cod_Municipio, Produto_Padronizado, SAFRA) %>% 
@@ -46,7 +45,7 @@ Proagro_soja_mun <- data_Proagro %>%
     VL_RECP = sum(VL_RECP),         #recursos próprios
     VL_INV = sum(VL_INV),           #parcela do investimento
     VL_GRM = sum(VL_GRM),           #reda mínima
-    VL_TOT = sum(VL_TOT),           #Valor total
+    VL_TOTAL = sum(VL_TOT),           #Valor total
     VL_Premio = sum(VL_ADIC),
     VL_SUBVENCAO_FEDERAL = 0,
     RECEITA_ESTIMADA = sum(RBE),
@@ -54,13 +53,13 @@ Proagro_soja_mun <- data_Proagro %>%
     VL_SINISTROS = sum(VL_COB_DEF)
   )
 Proagro_soja_mun$Area_Media               = Proagro_soja_mun$NR_AREA_TOTAL/Proagro_soja_mun$num_seguros
-Proagro_soja_mun$Valor_Total_Medio        = Proagro_soja_mun$VL_TOT/Proagro_soja_mun$num_seguros
+Proagro_soja_mun$VL_TOTAL_Medio           = Proagro_soja_mun$VL_TOTAL/Proagro_soja_mun$num_seguros
 Proagro_soja_mun$Premio_Liquido_Medio     = Proagro_soja_mun$VL_Premio/Proagro_soja_mun$num_seguros
 Proagro_soja_mun$Subvencao_Media          = Proagro_soja_mun$VL_SUBVENCAO_FEDERAL/Proagro_soja_mun$num_seguros
 Proagro_soja_mun$Receita_Media            = Proagro_soja_mun$RECEITA_ESTIMADA/Proagro_soja_mun$num_seguros
 Proagro_soja_mun$Premio_Area              = Proagro_soja_mun$VL_Premio/Proagro_soja_mun$NR_AREA_TOTAL
 Proagro_soja_mun$INDICE_SINISTRO          = Proagro_soja_mun$QT_SINISTROS/Proagro_soja_mun$num_seguros
-Proagro_soja_mun$VL_SINISTO_MEDIO         = Proagro_soja_mun$VL_SINISTROS/Proagro_soja_mun$QT_SINISTROS
+Proagro_soja_mun$VL_SINISTRO_MEDIO        = Proagro_soja_mun$VL_SINISTROS/Proagro_soja_mun$QT_SINISTROS
 Proagro_soja_mun$Tipo                     = "Proagro"
 
 
@@ -75,7 +74,7 @@ Proagro_soja_mun_programa <- data_Proagro %>%
     VL_RECP = sum(VL_RECP),         #recursos próprios
     VL_INV = sum(VL_INV),           #parcela do investimento
     VL_GRM = sum(VL_GRM),           #reda mínima
-    VL_TOT = sum(VL_TOT),           #Valor total
+    VL_TOTAL = sum(VL_TOT),           #Valor total
     VL_Premio = sum(VL_ADIC),
     VL_SUBVENCAO_FEDERAL = 0,
     RECEITA_ESTIMADA = sum(RBE),
@@ -85,25 +84,25 @@ Proagro_soja_mun_programa <- data_Proagro %>%
 
 
 Proagro_soja_mun_programa$Area_Media              = Proagro_soja_mun_programa$NR_AREA_TOTAL/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Valor_Total_Medio       = Proagro_soja_mun_programa$VL_TOT/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$Premio_Liquido_Medio    = Proagro_soja_mun_programa$VL_Premio/Proagro_soja_mun_programa$num_seguros
+Proagro_soja_mun_programa$VL_TOTAL_Medio          = Proagro_soja_mun_programa$VL_TOTAL/Proagro_soja_mun_programa$num_seguros
+Proagro_soja_mun_programa$VL_PREMIO_PAGO_Medio    = Proagro_soja_mun_programa$VL_Premio/Proagro_soja_mun_programa$num_seguros
 Proagro_soja_mun_programa$Subvencao_Media         = Proagro_soja_mun_programa$VL_SUBVENCAO_FEDERAL/Proagro_soja_mun_programa$num_seguros
 Proagro_soja_mun_programa$Receita_Media           = Proagro_soja_mun_programa$RECEITA_ESTIMADA/Proagro_soja_mun_programa$num_seguros
 Proagro_soja_mun_programa$Premio_Area             = Proagro_soja_mun_programa$VL_Premio/Proagro_soja_mun_programa$NR_AREA_TOTAL
 Proagro_soja_mun_programa$INDICE_SINISTRO         = Proagro_soja_mun_programa$QT_SINISTROS/Proagro_soja_mun_programa$num_seguros
-Proagro_soja_mun_programa$VL_SINISTO_MEDIO        = Proagro_soja_mun_programa$VL_SINISTROS/Proagro_soja_mun_programa$QT_SINISTROS
+Proagro_soja_mun_programa$VL_SINISTRO_MEDIO       = Proagro_soja_mun_programa$VL_SINISTROS/Proagro_soja_mun_programa$QT_SINISTROS
 
 
 PSR_Proagro_Union_Mun <- union_all(
-  x = Proagro_soja_mun %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
-  y = PSR_soja_mun %>%  select(-c("Produtividade_Seg_Media", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "NR_PRODUTIVIDADE_ESTIMADA"))
+  x = Proagro_soja_mun %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA", "VL_FIN",	"VL_RECP",	"VL_INV",	"VL_GRM")),
+  y = PSR_soja_mun %>%  select(-c("Produtividade_Seg_Media", "Produtividade_Media", "VL_PREMIO_LIQUIDO", "NR_PRODUTIVIDADE_SEGURADA", "NR_PRODUTIVIDADE_ESTIMADA"))
 )
 
 
 
 PSR_Proagro_Union_Mun_Programa <- union_all(
-  x = Proagro_soja_mun_programa %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA")),
-  y = PSR_soja_mun_programa %>%  select(-c("Produtividade_Seg_Media", "Produtividade_Media", "NR_PRODUTIVIDADE_SEGURADA", "NR_PRODUTIVIDADE_ESTIMADA"))
+  x = Proagro_soja_mun_programa %>%  select ( -c("Receita_Media", "RECEITA_ESTIMADA", "VL_FIN",	"VL_RECP",	"VL_INV",	"VL_GRM")),
+  y = PSR_soja_mun_programa %>%  select(-c("Produtividade_Seg_Media", "Produtividade_Media", "VL_PREMIO_LIQUIDO", "NR_PRODUTIVIDADE_SEGURADA", "NR_PRODUTIVIDADE_ESTIMADA"))
 )
 
 
@@ -113,16 +112,6 @@ PSR_Proagro_lado_lado_mun <-  left_join(
   y = Proagro_soja_mun,
   by =c("Cod_Municipio" = "Cod_Municipio")
 )
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -164,13 +153,13 @@ Proagro_soja_regiao <- data_Proagro %>%
   summarise(
     num_seguros = sum(QUANTIDADE),
     NR_AREA_TOTAL = sum(AREA),
-    VL_LIMITE_GARANTIA = sum(VALOR_TOTAL),
+    VALOR_TOTAL = sum(VL_TOT),
     VL_PREMIO_LIQUIDO = sum(VALOR_ADICIONAL),
     VL_SUBVENCAO_FEDERAL = 0,
     RECEITA_ESTIMADA = sum(RBE)
   )
 Proagro_soja_regiao$Area_Media               = Proagro_soja_regiao$NR_AREA_TOTAL/Proagro_soja_regiao$num_seguros
-Proagro_soja_regiao$Limite_Medio             = Proagro_soja_regiao$VL_LIMITE_GARANTIA/Proagro_soja_regiao$num_seguros
+Proagro_soja_regiao$Limite_Medio             = Proagro_soja_regiao$VALOR_TOTAL/Proagro_soja_regiao$num_seguros
 Proagro_soja_regiao$Premio_Liquido_Medio     = Proagro_soja_regiao$VL_PREMIO_LIQUIDO/Proagro_soja_regiao$num_seguros
 Proagro_soja_regiao$Subvencao_Media          = Proagro_soja_regiao$VL_SUBVENCAO_FEDERAL/Proagro_soja_regiao$num_seguros
 Proagro_soja_regiao$Receita_Media            = Proagro_soja_regiao$RECEITA_ESTIMADA/Proagro_soja_regiao$num_seguros
---
+++ Scripts/Estatisticas_PSR.R
@@ -0,0 +1,18 @@
+source("./Scripts/01_Load_Data.R", encoding = "UTF-8")
+source("./Scripts/02_Transformação.R", encoding = "UTF-8")
+source("./Scripts/Function_Gerais.R", encoding = "UTF-8")
+library(ggplot2)
+library(dplyr)
+library(summarytools)
+
+
+estat_PSR <- data_PSR %>% filter(Produto_Padronizado == "SOJA", VL_LIMITE_GARANTIA <= 300000) 
+estat_PSR$Premio_Area <- estat_PSR$VL_PREMIO_LIQUIDO/estat_PSR$NR_AREA_TOTAL
+
+estat_PSR <- remove_outliers(estat_PSR)
+
+
+#Ver package outliers
+
+
+estat_PSR[estat_PSR$Premio_Area == max(estat_PSR$Premio_Area),]
