4d10ba05e54989afd077564429807c4745ad5586
VitorVillarino <vitor.villarino@gmail.com>
2019-08-16 17:43:02 -0300

    #COMMIT 16/08

--- Scripts/Estatisticas_PSR.R
+++ Scripts/Estatisticas_PSR.R
@@ -1,6 +1,5 @@
 source("./Scripts/01_Load_Data.R", encoding = "UTF-8")
 source("./Scripts/02_Transformação.R", encoding = "UTF-8")
-source("./Scripts/Function_Gerais.R", encoding = "UTF-8")
 library(ggplot2)
 library(dplyr)
 library(summarytools)
@@ -8,11 +7,13 @@ library(summarytools)
 
 estat_PSR <- data_PSR %>% filter(Produto_Padronizado == "SOJA", VL_LIMITE_GARANTIA <= 300000) 
 estat_PSR$Premio_Area <- estat_PSR$VL_PREMIO_LIQUIDO/estat_PSR$NR_AREA_TOTAL
+estat_PSR <- estat_PSR[estat_PSR$Premio_Area < 400,]
+estat_PSR <- estat_PSR[estat_PSR$Premio_Area > 7,]
+estat_PSR <- estat_PSR[estat_PSR$NR_AREA_TOTAL < 750,]
 
-estat_PSR <- remove_outliers(estat_PSR)
+descr(estat_PSR$NR_AREA_TOTAL)
+openxlsx::write.xlsx(estat_PSR, "estat_PSR.xlsx")
 
 
-#Ver package outliers
-https://cran.r-project.org/web/packages/outliers/outliers.pdf
 
-estat_PSR[estat_PSR$Premio_Area == max(estat_PSR$Premio_Area),]
+esquisse::esquisser()
